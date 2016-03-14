b782fc4a17dde56094b88f23364aa9ff5d3c4731
Benjamin 'rasben' Rasmussen <rasben@users.noreply.github.com>
2016-03-14 13:27:34 +0100

    Moved rasben theme to seperate repo

--- themes/custom/rasben/gruntfile.js
+++ themes/custom/rasben/gruntfile.js
@@ -1,7 +1,40 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('build', ['compass']);
+  grunt.registerTask('build', ['clean:pre', 'compass', 'uglify']);
 
-  })
+  grunt.initConfig({
+    uglify: {
+      js: {
+        files: {
+          'js/build/base.min.js': ['js/source/*']
+        }
+      }
+    },
+
+    clean: {
+      pre: {
+        src: ['js/build/*', '!js/build', 'styles/css/*', '!styles/css/'],
+      },
+     post: {
+        src: [''],
+      }
+    },
+
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'styles/sass',
+          cssDir: 'styles/css',
+          environment: 'production',
+          outputStyle: 'compressed',
+          config: 'config.rb'
+        }
+      },
+    },
+
+  });
 }
+
---
+++ themes/custom/rasben/readme.md
@@ -0,0 +1,9 @@
+Rasben Bare Theme for Drupal7
+============================
+
+Can be used in accordance with the D7-rasben-bare starter site.
+
+
+This theme uses Grunt, SASS (SCSS syntax) and will use various other stuff in the future.
+
+Never edit files directly in styles/build and js/build. ALWAYS edit the source files in styles/source js/source and run the grunt build command to compile.
